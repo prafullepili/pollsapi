f549695d225c6ec3b8c76c6125c626a0176a037c
Prafull Epili <prafullepili@gmail.com>
2023-02-01 12:47:25 +0530

    gitignore

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,12 +1,14 @@
-from rest_framework import serializers
 
+#pylint:disable=E0402
+#pylint:disable=C0115
+from rest_framework import serializers
 from .models import Poll, Choice, Vote
 
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
-        fields = "__all__"
+        fields = '__all__'
 
 
 class ChoiceSerializer(serializers.ModelSerializer):
--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,15 @@
 #pylint:disable=E0402
 from django.urls import path
 from .views import polls_list, polls_detail
-from .apiviews import PollList, PollDetail
+from .apiviews import PollList, PollDetail, CreateVote, ChoiceList
 
 urlpatterns = [
     path('polls/', polls_list, name="polls_list"),
     path('polls/<int:pk>/', polls_detail, name="polls_detail"),
     path('pollsapi/', PollList.as_view(), name='pollsapi_list'),
     path('pollsapi/<int:pk>/', PollDetail.as_view(), name="pollsapi_detail"),
+    
+    path('choices/',ChoiceList.as_view(),name='choice_list'),
+    path('vote/',CreateVote.as_view(),name="create_vote"),
+    
 ]
